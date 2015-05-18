f68e3f8fa841d8f5942a6a87e2e60cff83082a75
Leonardo Balter <leonardo.balter@gmail.com>
2015-05-18 15:46:30 -0400

    Fix model tests

--- app/models/imageable.js
+++ app/models/imageable.js
@@ -3,7 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr(),
   dateOfBirth: DS.attr('date'),
-  dateOfDeath: DS.attr('date'),
-  books: DS.hasMany('book'),
-  photos: DS.hasMany('photo')
+  dateOfDeath: DS.attr('date')
 });
--- tests/unit/models/store-test.js
+++ tests/unit/models/store-test.js
@@ -1,12 +1,28 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('store', 'Unit | Model | store', {
   // Specify the other units that are required for this test.
-  needs: ['model:book']
+  needs: [
+    'model:book',
+    'model:chapter',
+    'model:series',
+    'model:photo',
+    'model:author'
+  ]
 });
 
 test('it exists', function(assert) {
   var model = this.subject();
   // var store = this.store();
   assert.ok(!!model);
+});
+
+test('books relationship', function(assert) {
+  var request = this.store().modelFor('store');
+
+  var relationship = Ember.get(request, 'relationshipsByName').get('books');
+
+  assert.equal(relationship.key, 'books');
+  assert.equal(relationship.kind, 'hasMany');
 });
