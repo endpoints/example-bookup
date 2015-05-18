00211400bb04aa0c288a26d1203d0fc56f1628bf
Leonardo Balter <leonardo.balter@gmail.com>
2015-05-18 17:30:35 -0400

    Book page

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('book', {path: '/book/:id'});
 });
 
 export default Router;
--- app/routes/book.js
+++ app/routes/book.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
+  model: function(params) {
     return this.store.find('book', {
       include: ['chapters', 'photos', 'author'].join(','),
-      filter: {
-        // published_after: '1990-01-01'
-      }
+      id: params.id
+    }).then(function(data) {
+      return data.objectAt(0);
     });
   }
 });
